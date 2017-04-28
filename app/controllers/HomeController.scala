5d477c3da39c7d8bebf4c60a999557dbf4063b7f
Bard Wang <Bard Wang>
2017-04-28 17:50:53 -0400

    m

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -31,20 +31,11 @@ class HomeController @Inject() extends Controller{
       .config("spark.sql.warehouse.dir", "somevalue")
       .getOrCreate()
 
-
-    val x = (request.body.get("ypos").map(_.head).get.toInt - 250).toDouble / 250
-    val y = (request.body.get("xpos").map(_.head).get.toDouble - 235) / 470
+    val x = (request.body.get("xpos").map(_.head).get.toDouble) / 470
+    val y = (request.body.get("ypos").map(_.head).get.toInt - 250).toDouble / 250
     val d_distance = request.body.get("defenderdistance").map(_.head).get.toInt
-    val score_margin = request.body.get("scoremargin").map(_.head).get.toInt
+    val score_margin = request.body.get("scoremargin").map(_.head).get.toDouble / 30
     val period = request.body.get("period").map(_.head).get.toInt
-
-    /*
-    val x = 0.5
-    val y = 0.6
-    val d_distance = 0
-    val score_margin = 2
-    val period = 1
-    */
 
     // Convert 1-based indices to 0-based.
     val d_distance_onehot = Vectors.sparse(7, Array(d_distance), Array(1))
@@ -54,23 +45,23 @@ class HomeController @Inject() extends Controller{
       (x, y, d_distance_onehot, score_margin, period_onehot)
     )).toDF("x", "y", "d_distance_onehot", "score_margin", "period_onehot")
 
-    // df_prediction.show
+    //df_prediction.show
 
     val assembler = new VectorAssembler()
       .setInputCols(Array("x", "y", "d_distance_onehot", "score_margin", "period_onehot"))
-      .setOutputCol("indexedFeatures")
+      .setOutputCol("features")
 
     val output_1 = assembler.transform(df_prediction)
 
-    val sameModel = RandomForestRegressionModel.read.load("rfModel")
+    val sameModel =RandomForestRegressionModel.read.load("rfModel_final")
     val predictions_test = sameModel.transform(output_1)
-    val prediction = predictions_test.select("prediction")
-
+    val prediction = predictions_test.select("prediction").head().getDouble(0)
+    println( "%.1f".format(prediction * 100 ) + "%")
     predictions_test.show
+
     val result = predictions_test.select("prediction").first().get(0).toString
     val resultpercent = (result.toDouble * 100).toInt.toString
 
     Ok(views.html.index(resultpercent))
   }
-
 }
